0c183d6680728b3332bb6f598ac200220c607ebc
Cindy Sun <csun76@smith.edu>
2023-12-01 16:01:12 -0500

    for Katy

--- R/citation.R
+++ R/citation.R
@@ -2,6 +2,13 @@ library(rvest)
 library(httr)
 library(jsonlite)
 
+
+# example
+name <- c("Recognition and prevention of child abuse in the child with disability")
+key <- c("8efa1b058c869298ced0ed407f3886f9ca45042acdd23691e48b6bced92d3652")
+
+
+
 search <- function(name, key) {
 #User need to input the name of the article and the API key in order to search
   search_url <- paste0("https://serpapi.com/search?engine=google_scholar&q=",
@@ -10,10 +17,17 @@ search <- function(name, key) {
   )
   article <- read_json(search_url)
 
-  next_url <- article$organic_results[[1]]$inline_links$serpapi_cite_link
+  serpapi_cite_link <- article$organic_results[[1]]$inline_links$serpapi_cite_link
 
-  citation_json <- read_json(paste0(next_url, "&api_key=", key))
+  citation_json <- read_json(paste0(serpapi_cite_link, "&api_key=", key))
 
   citation_json$citations
-  #citation_json$links
+  BibTex_html <- citation_json[["links"]][[1]][["link"]]
+  BibTex <- read_html(paste0(BibTex_html, "&api_key=", key))
+  citation_json[["citations"]][[2]][["snippet"]]
+
 }
+
+
+
+search("Recognition and prevention of child abuse in the child with disability", "8efa1b058c869298ced0ed407f3886f9ca45042acdd23691e48b6bced92d3652")
