29d49d2536ff97807a308f8405663741a25404bb
Cindy Sun <csun76@smith.edu>
2023-12-06 16:03:40 -0500

    update name for API in unit test check

--- tests/testthat/test-citation_test.R
+++ tests/testthat/test-citation_test.R
@@ -1,11 +1,11 @@
-api_cindy <- readLines("api_example.txt")
+api_example <- readLines("api_example.txt")
 
 test_that("the APA function creates a character string", {
-  expect_equal(is.character(APA("Homeostasis, stress, trauma, and adaptation: A neurodevelopmental view of childhood trauma", api_cindy)),
+  expect_equal(is.character(APA("Homeostasis, stress, trauma, and adaptation: A neurodevelopmental view of childhood trauma", api_example)),
               TRUE)
 })
 
 test_that("the APA function creates an APA citation", {
-  expect_equal(APA("Homeostasis, stress, trauma, and adaptation: A neurodevelopmental view of childhood trauma", api_cindy),
+  expect_equal(APA("Homeostasis, stress, trauma, and adaptation: A neurodevelopmental view of childhood trauma", api_example),
                "Perry, B. D., & Pollard, R. (1998). Homeostasis, stress, trauma, and adaptation: A neurodevelopmental view of childhood trauma. Child and Adolescent Psychiatric Clinics, 7(1), 33-51.")
 })
