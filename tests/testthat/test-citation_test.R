26152bd7f7eb105845d72ee881ee29359363edde
Cindy Sun <csun76@smith.edu>
2023-12-12 13:10:55 -0500

    add citation secret

--- tests/testthat/test-citation_test.R
+++ tests/testthat/test-citation_test.R
@@ -1,4 +1,8 @@
-api_example <- readLines("api_example.txt")
+if (Sys.getenv("GITHUB_ACTIONS") == "true") {
+  api_example <- Sys.getenv("API_KEY")
+} else {
+  api_example <- readLines("api_example.txt")
+}
 
 test_that("the APA function creates a character string", {
   expect_equal(is.character(APA("Homeostasis, stress, trauma, and adaptation: A neurodevelopmental view of childhood trauma", api_example)),
